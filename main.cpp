94dbef7a4aca1d73f97264327dda4dc42cea2118
Tyler Brown <tybr3595@colorado.edu>
2021-04-17 21:50:33 -0600

    lksdjf

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+//Leeb's bruh knowledge:
 //To make changes to the repo
 //make the change
 //In terminal
 //git add .
 //git commit -m "message"
 //git push
+
+//always git pull before editing and usually before get push (if get error)
